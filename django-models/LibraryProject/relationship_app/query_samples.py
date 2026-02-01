ba7d3f5f7040cfd53abc3dc2144161f513a54da1
Laura Neema <laurajuma52@gmail.com>
2026-02-01 16:49:15 +0300

    Fix bug

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -3,13 +3,13 @@ from relationship_app.models import Author, Book, Library, Librarian
 author = Author.objects.get(name ="Jon Snow")
 books_by_author = author.books.all
 
-library = Library.objects.get(name="")
+library = Library.objects.get(name="library_name")
 books_in_library = library.books.all()
 
 for book in books_in_library:
   print(book.title)
 
 
-library = Library.objects.get(name="")
+library = Library.objects.get(name="library_name")
 library.librarian
 
