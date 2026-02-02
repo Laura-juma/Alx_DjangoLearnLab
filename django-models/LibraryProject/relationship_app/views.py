00a7168f43b93cbbc5464f1a46f65fe8ed57672f
Laura Neema <laurajuma52@gmail.com>
2026-02-02 16:21:19 +0300

    Add user authentication

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -20,6 +20,15 @@ class LibraryDetailView(DetailView):
   template_name = 'relationship_app/library_detail.html'
   context_object_name = 'library'
 
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+class SignUpView(CreateView):
+  form_class = UserCreationForm
+  success_url = reverse_lazy('login')
+  template_name = 'relationship_app/register.html'
+
   
   
 
