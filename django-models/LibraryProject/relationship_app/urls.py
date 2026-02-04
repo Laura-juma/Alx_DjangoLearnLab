dab7da2946ff5f35188e8b4503f646fc20af0825
Laura Neema <laurajuma52@gmail.com>
2026-02-04 19:58:38 +0300

    Fix bugs;

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -37,9 +37,10 @@ urlpatterns = [
     path('admin/', admin_view, name='admin_view'),
     path('librarian/', librarian_view, name='librarian_view'),
     path('member/', member_view, name='member_view'),
-     path('books/add/', views.add_book_view, name='add_book'),
-    path('books/edit/<int:pk>/', views.edit_book_view, name='edit_book'),
-    path('books/delete/<int:pk>/', views.delete_book_view, name='delete_book'),
+     path('add_book/', views.add_book_view, name='add_book'),
+    path('edit_book/<int:pk>/', views.edit_book_view, name='edit_book'),
+    path('delete_book/<int:pk>/', views.delete_book_view, name='delete_book'),
+
 ]  
 
 
