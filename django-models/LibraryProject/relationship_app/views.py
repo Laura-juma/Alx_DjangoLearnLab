0203291ab62ac008d822859935e86cf40cfb315e
Laura Neema <laurajuma52@gmail.com>
2026-02-04 19:33:33 +0300

    Add user_passes_test decorator to views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import user_passes_test
 
 def list_books_view(request):
   books =Book.objects.all()
@@ -45,50 +46,41 @@ from django.contrib.auth import authenticate
   if user:
     login(request, user)"""
 
+def is_admin(user):
+    return user.userprofile.role == 'ADM'
+
+def is_librarian(user):
+    return user.userprofile.role == 'LIB'
+
+def is_member(user):
+    return user.userprofile.role == 'MEM'
+
+
 @login_required
+@user_passes_test(is_admin, login_url='no_access')
 def admin_view(request):
-  if UserProfile.role != 'ADM':
-    return redirect('no_access')
-  
-  context = {
-    'message': 'Welcome to the admin page.'
-  }
-
-  return render(request, 'relationship_app/admin_view.html', context)
+    context = {
+        'message': 'Welcome to the admin page.'
+    }
+    return render(request, 'relationship_app/admin_view.html', context)
 
 
 @login_required
-def admin_view(request):
-  if request.user.userprofile.role != 'LIB':
-    return redirect('no_access')
-  
-  context = {
-    'message': 'Welcome to the Librarian page.'
-  }
-
-  return render(request, 'relationship_app/admin_view.html', context)
-
-@login_required
+@user_passes_test(is_librarian, login_url='no_access')
 def librarian_view(request):
-  if request.user.userprofile.role != 'LIB':
-    return redirect('no_access')
-  
-  context = {
-    'message': 'Welcome to the Librarian page.'
-  }
+    context = {
+        'message': 'Welcome to the Librarian page.'
+    }
+    return render(request, 'relationship_app/librarian_view.html', context)
 
-  return render(request, 'relationship_app/librarian_view.html', context)
 
 @login_required
+@user_passes_test(is_member, login_url='no_access')
 def member_view(request):
-  if request.user.userprofile.role != 'MEM':
-    return redirect('no_access')
-  
-  context = {
-    'message': "Welcome to the Member's page."
-  }
-
-  return render(request, 'relationship_app/member_view.html', context)
+    context = {
+        'message': "Welcome to the Member's page."
+    }
+    return render(request, 'relationship_app/member_view.html', context)
 
 
 
