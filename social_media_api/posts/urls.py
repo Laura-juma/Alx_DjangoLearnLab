b4c27c648866afc92b37815150c99d2324dcb9f2
Laura Neema <laurajuma52@gmail.com>
2026-03-01 20:31:33 +0300

    Fix bugs

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -3,19 +3,12 @@ from rest_framework.routers import DefaultRouter
 from .views import PostViewSet, CommentViewSet, feed, like_post, unlike_post
 
 router = DefaultRouter()
-
 router.register(r'posts', PostViewSet)
 router.register(r'comments', CommentViewSet)
 
 urlpatterns = [
-
     path('', include(router.urls)),
-
     path('feed/', feed),
-
-
     path('posts/<int:pk>/like/', like_post),
-
     path('posts/<int:pk>/unlike/', unlike_post),
-
 ]
--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,76 +1,47 @@
 from rest_framework import viewsets, permissions, generics
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from django.contrib.contenttypes.models import ContentType
 
-from .models import Post, Comment
+from .models import Post, Comment, Like
 from .serializers import PostSerializer, CommentSerializer
-
+from notifications.models import Notification
 
 # POST VIEWSET
 class PostViewSet(viewsets.ModelViewSet):
-
     queryset = Post.objects.all().order_by('-created_at')
-
     serializer_class = PostSerializer
-
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
-
         serializer.save(author=self.request.user)
-
 
 # COMMENT VIEWSET
 class CommentViewSet(viewsets.ModelViewSet):
-
     queryset = Comment.objects.all().order_by('-created_at')
-
     serializer_class = CommentSerializer
-
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
-
         serializer.save(author=self.request.user)
 
-
-# FEED VIEW 
+# FEED VIEW
+@api_view(['GET'])
 @permission_classes([permissions.IsAuthenticated])
 def feed(request):
-
     following_users = request.user.following.all()
-
-   
     posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
-
     serializer = PostSerializer(posts, many=True)
-
     return Response(serializer.data)
 
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.response import Response
-from rest_framework import permissions
-from django.shortcuts import get_object_or_404
-
-from .models import Post, Like
-from notifications.models import Notification
-from django.contrib.contenttypes.models import ContentType
-
-
+# LIKE POST VIEW
 @api_view(['POST'])
 @permission_classes([permissions.IsAuthenticated])
 def like_post(request, pk):
-
-    post = get_object_or_404(Post, pk=pk)
-
-    like, created = Like.objects.get_or_create(
-        user=request.user,
-        post=post
-    )
+    post = generics.get_object_or_404(Post, pk=pk)
+    like, created = Like.objects.get_or_create(user=request.user, post=post)
 
     if created:
-
         Notification.objects.create(
             recipient=post.author,
             actor=request.user,
@@ -78,21 +49,14 @@ def like_post(request, pk):
             content_type=ContentType.objects.get_for_model(post),
             object_id=post.id
         )
-
         return Response({"message": "Post liked"})
 
     return Response({"message": "Already liked"})
 
-
+# UNLIKE POST VIEW
 @api_view(['POST'])
 @permission_classes([permissions.IsAuthenticated])
 def unlike_post(request, pk):
-
-    post = get_object_or_404(Post, pk=pk)
-
-    Like.objects.filter(
-        user=request.user,
-        post=post
-    ).delete()
-
+    post = generics.get_object_or_404(Post, pk=pk)
+    Like.objects.filter(user=request.user, post=post).delete()
     return Response({"message": "Post unliked"})
