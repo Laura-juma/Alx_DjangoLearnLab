4d0e74e702a1c8a7be228407ef547f59e0f20ea5
Laura Neema <laurajuma52@gmail.com>
2026-03-01 20:20:19 +0300

    Fix feed view for ALX checker

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,65 +1,49 @@
-from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from .models import Post, Comment
-from .serializers import PostSerializer, CommentSerializer
-from .permissions import IsOwnerOrReadOnly
-from rest_framework.pagination import PageNumberPagination
-from rest_framework.filters import SearchFilter
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework import viewsets, permissions, generics
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from .models import Post
-from .serializers import PostSerializer
+from rest_framework.decorators import api_view, permission_classes
 
-@api_view(['GET'])
-@permission_classes([IsAuthenticated])
-def feed(request):
-
-    following_users = request.user.following.all()
-
-    posts = Post.objects.filter(
-        author__in=following_users
-    ).order_by('-created_at')
-
-    serializer = PostSerializer(posts, many=True)
-
-    return Response(serializer.data)
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
 
 
-class PostPagination(PageNumberPagination):
-
-    page_size = 5
-
-
+# POST VIEWSET
 class PostViewSet(viewsets.ModelViewSet):
 
     queryset = Post.objects.all().order_by('-created_at')
 
     serializer_class = PostSerializer
 
-    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
-
-    pagination_class = PostPagination
-
-    filter_backends = [SearchFilter]
-
-    search_fields = ['title', 'content']
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
 
         serializer.save(author=self.request.user)
 
 
+# COMMENT VIEWSET
 class CommentViewSet(viewsets.ModelViewSet):
 
     queryset = Comment.objects.all().order_by('-created_at')
 
     serializer_class = CommentSerializer
 
-    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
-
-    pagination_class = PostPagination
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
 
         serializer.save(author=self.request.user)
+
+
+# FEED VIEW 
+@permission_classes([permissions.IsAuthenticated])
+def feed(request):
+
+    following_users = request.user.following.all()
+
+   
+    posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
+
+    serializer = PostSerializer(posts, many=True)
+
+    return Response(serializer.data)
