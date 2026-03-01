3991e024c434d062a38d90741c34268eb35ce75c
Laura Neema <laurajuma52@gmail.com>
2026-03-01 21:02:38 +0300

    Add authentication

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,5 +1,54 @@
-from django.shortcuts import render
+
 from .models import Post
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from .forms import CustomUserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages
+
+# Registration
+def register_view(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+        else:
+            messages.error(request, 'Registration failed. Please check the form.')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+# Login
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('index')
+        else:
+            messages.error(request, 'Invalid username or password.')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'blog/login.html', {'form': form})
+
+# Logout
+def logout_view(request):
+    logout(request)
+    return redirect('index')
+
+# Profile
+@login_required
+def profile_view(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        request.user.email = email
+        request.user.save()
+        messages.success(request, 'Profile updated successfully.')
+    return render(request, 'blog/profile.html')
 
 def index(request):
     posts = Post.objects.all()
